0e48d8b17afa506d7dc716af6a1d21c66ebc9995
sukrubeyy <sukru.beyy@outlook.com>
2021-04-09 00:35:43 +0300

    Add project

---
+++ C#_VoiceBot/VoiceBot/VoiceBot/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace VoiceBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Speak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Speak
+            // 
+            this.Speak.Location = new System.Drawing.Point(343, 103);
+            this.Speak.Name = "Speak";
+            this.Speak.Size = new System.Drawing.Size(132, 83);
+            this.Speak.TabIndex = 0;
+            this.Speak.Text = "Speak";
+            this.Speak.UseVisualStyleBackColor = true;
+            this.Speak.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Speak);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Speak;
+    }
+}
+
---
+++ C#_VoiceBot/VoiceBot/VoiceBot/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+
+
+namespace VoiceBot
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            repeat();
+        }
+
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+        PromptBuilder pbuilder = new PromptBuilder();
+        SpeechRecognitionEngine rEngine = new SpeechRecognitionEngine();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        void repeat()
+        {
+            Choices commands = new Choices();
+            commands.Add(new String[] {"open opera","open twitter","open telegram","music","open education",
+            "open counter strike globbal offensive","open visual studio code","open visual studio","open unity","open blender",
+            "standart","open news","open medium"
+            });
+            Grammar gramer = new Grammar(new GrammarBuilder(commands));
+            rEngine.RequestRecognizerUpdate();
+            rEngine.LoadGrammar(gramer);
+            rEngine.SpeechRecognized += REngine_SpeechRecognized;
+            rEngine.SetInputToDefaultAudioDevice();
+            rEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void REngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                
+                case "open twitter":
+                    {
+                        pbuilder.ClearContent();
+                        pbuilder.AppendText("Twitter opening");
+                        synt.Speak(pbuilder);
+                        Process.Start("www.twitter.com");
+                        break;
+                    }
+             
+                case "open news":
+                    {
+                        pbuilder.ClearContent();
+                        pbuilder.AppendText("BBC News Opening");
+                        synt.Speak(pbuilder);
+                        Process.Start("https://www.bbc.com/news");
+                        break;
+                    }
+
+                 
+                case "open medium":
+                    {
+                        pbuilder.ClearContent();
+                        pbuilder.AppendText("Meduim Opening");
+                        synt.Speak(pbuilder);
+                        Process.Start("https://sukrubeyy.medium.com");
+                        break;
+                    }
+            }
+        }
+    }
+}
